3cf09a314282b0858cb3c92c647116a4a13019ba
Taeyong98 <jang880022@gmail.com>
2024-06-11 16:50:33 +0900

    Revert "네비게이션 추가"
    This reverts commit f4c5e8acb5ad238609c46b4d176bb5b311ee2c97.

--- ahcha-ahcha-app/src/main.js
+++ ahcha-ahcha-app/src/main.js
@@ -1,9 +1,8 @@
-// import './assets/main.css'
+import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 import App from './App.vue'
 import router from './router'
 
