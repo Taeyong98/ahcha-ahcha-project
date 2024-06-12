5ca6e13f81bf6c580aca18878e7d90cc3520e42c
soyoung1832 <68853692+soyoung1832@users.noreply.github.com>
2024-06-12 12:50:16 +0900

    emit 추가 및 price 코드 추가

--- ahcha-ahcha-app/src/views/HomeView.vue
+++ ahcha-ahcha-app/src/views/HomeView.vue
@@ -1,12 +1,27 @@
+<template>
+  
+    <TransactGraph ref="transactGraph"/>
+    <TransactTrack ref="transactTrack"/>
+    <!-- <WriteButton @logChanged="handleLogChanged"></WriteButton> -->
+  
+</template>
+
 <script setup>
+import { ref } from 'vue';
 import TransactTrack from '@/components/Home/TransactTrack.vue';
 import TransactGraph from '@/components/Home/TransactGraph.vue';
+// import WriteButton from '@/components/WriteButton.vue';
 
+const transactGraph = ref(null);
+const transactTrack = ref(null);
+
+const handleLogChanged = () => {
+  if (transactGraph.value) {
+    transactGraph.value.updateGraph();
+  }
+
+  if (transactTrack.value) {
+    transactTrack.value.updateList();
+  }
+};
 </script>
-
-<template>
-  <main>
-    <TransactGraph/>
-    <TransactTrack/>
-  </main>
-</template>
