cbe3d76ff42744141809807b82fb29b0bafd4abe
soyoung1832 <68853692+soyoung1832@users.noreply.github.com>
2024-06-12 16:03:13 +0900

    nav수정3

--- ahcha-ahcha-app/src/components/Home/Navbar.vue
+++ ahcha-ahcha-app/src/components/Home/Navbar.vue
@@ -2,28 +2,22 @@
   <header>
     <nav class="navbar">
       <div class="container">
-        <h1 class="navbar-brand">앗차앗차</h1>
-        
+        <span class="navbar-brand">앗차앗차</span>
         <ul class="nav">
           <li class="inButton">
-            <router-link to="/">Home</router-link>
-           
+            <router-link to="/ahcha/">홈</router-link>
           </li>
           <li class="inButton">
-           
-            <router-link to="/">Info</router-link>
-
+            <router-link to="/ahcha/trade">거래</router-link>
           </li>
           <li class="inButton">
-           
-            <router-link to="/">Ahcha Challenge</router-link>
+            <router-link to="/ahcha/challenge">앗차챌린지</router-link>
           </li>
-          
-          <li>
-            <router-link to="/"><img src="../../assets/navbar/profile.png" alt=""/></router-link>
+          <li class="icon">
+            <router-link to="/ahcha/mypage/:user_id"><img src="../../assets/navbar/profile.png" alt="Profile"/></router-link>
           </li>
-          <li>
-            <router-link to="/"><img src="../../assets/navbar/setting.png" alt=""/></router-link>
+          <li class="icon">
+            <router-link to=""><img src="../../assets/navbar/setting.png" alt="Setting"/></router-link>
           </li>
         </ul>
       </div>
@@ -31,45 +25,42 @@
   </header>
 </template>
 
-<style>
-
-
+<style scoped>
 /* 네비게이션 바 스타일 */
 .navbar {
-
   background-color: #FBE4A7;
   height: 70px;
   display: flex;
   align-items: center;
   padding: 0 10px;
-
 }
 
 /* 홈페이지 이름 스타일 */
 .navbar-brand {
-  
   font-size: 24px;
   font-weight: bold;
 }
 
 /* 컨테이너 스타일 */
 .container {
-
+  display: flex;
   justify-content: space-between;
+  align-items: center;
   width: 100%;
   max-width: 1200px;
 }
 
 /* 내비게이션 스타일 */
 .nav {
- 
   list-style: none;
   display: flex;
   align-items: center;
+  margin: 0;
+  padding: 0;
 }
 
 .nav li {
-  padding: 0 20px;
+  padding: 0 10px; /* 패딩을 줄여서 공간 확보 */
 }
 
 .nav li a {
@@ -77,12 +68,47 @@
   text-decoration: none;
 }
 
-
-
-
 /* 이미지 스타일 */
-img {
+.nav .icon img {
   width: 30px;
   height: 30px;
 }
+
+/* 반응형 디자인을 위한 미디어 쿼리 */
+@media (max-width: 520px) {
+  .navbar {
+    height: auto;
+    padding: 5px;
+  }
+
+  .container {
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .nav {
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    width: 100%;
+  }
+
+  .nav li {
+    padding: 5px; /* 패딩을 줄여서 공간 확보 */
+    text-align: center;
+    flex: 1 1 1; /* 요소들이 동일한 비율로 늘어나도록 설정 */
+  }
+
+  .navbar-brand {
+    margin-bottom: 10px;
+    font-size: 20px;
+  }
+
+  .nav .icon img {
+    width: 20px;
+    height: 20px; /* 이미지 크기 줄이기 */
+  }
+}
+
 </style>
+
