4210ce327ff6e6897b40856fa35548ca354d3b4b
soyoung1832 <68853692+soyoung1832@users.noreply.github.com>
2024-06-12 09:46:50 +0900

    TransactTrack.vue 업데이트2

--- ahcha-ahcha-app/src/views/HomeView.vue
+++ ahcha-ahcha-app/src/views/HomeView.vue
@@ -1,7 +1,12 @@
 <script setup>
+import TransactTrack from '@/components/Home/TransactTrack.vue';
+import TransactGraph from '@/components/Home/TransactGraph.vue';
+
 </script>
 
 <template>
   <main>
+    <TransactGraph/>
+    <TransactTrack/>
   </main>
 </template>
