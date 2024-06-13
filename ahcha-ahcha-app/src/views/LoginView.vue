2b3ca33c1c6b7be0a3ef05a1ffe0d73c3fa4fe18
Taeyong98 <jang880022@gmail.com>
2024-06-13 15:19:18 +0900

    temp commit

--- ahcha-ahcha-app/src/views/LoginView.vue
+++ ahcha-ahcha-app/src/views/LoginView.vue
@@ -1,34 +1,73 @@
 <template lang="">
-    <div class="mt-5 d-flex justify-content-center" >
+    <div class="mt-5 d-flex justify-content-center">
         <form>
             <div class="mb-1">
-                <input type="text" class="form-control" v-model="id_input" maxlength="14" placeholder="ID"/>
+                <input type="text" class="form-control" v-model="id_input" maxlength="14" placeholder="ID" />
             </div>
             <div class="mb-3">
-                <input type="password" class="form-control" id="exampleInputPassword1" v-model="pw_input" maxlength="14" placeholder="Password"/>
+                <input
+                    type="password"
+                    class="form-control"
+                    id="exampleInputPassword1"
+                    v-model="pw_input"
+                    maxlength="14"
+                    placeholder="Password"
+                />
             </div>
-            <div class="mb-3 form-check">
-                
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        
+            <div class="mb-3 form-check"></div>
+            <button type="button" class="m-1 btn" style="background-color: #fbe4a7" @click="login">로그인</button>
+            <button type="button" class="m-1 btn" style="background-color: #fbe4a7" @click="signUp">회원가입</button>
         </form>
     </div>
 </template>
 <script>
 import { ref, reactive } from 'vue';
-import axios from "axios";
+import axios from 'axios';
 import { useRouter, useRoute } from 'vue-router';
 export default {
     setup() {
         const router = useRouter();
         const route = useRoute();
 
-        const id_input = ref("");
-        const pw_input = ref("");
-        
+        const id_input = ref('');
+        const pw_input = ref('');
 
-        return{id_input, pw_input};
+        const login = async function () {
+            sessionStorage.setItem('userid', '');
+            try {
+                let response = await axios.get(`http://localhost:3001/user_list`);
+                let tempUser = response.data.find((u) => {
+                    return u.id == id_input.value;
+                });
+
+                if (tempUser == null) {
+                    alert('해당하는 id가 없습니다.');
+                    return;
+                }
+                if (tempUser.password != pw_input.value) {
+                    alert('password가 올바르지 않습니다!');
+                    return;
+                }
+
+                sessionStorage.setItem('userid', tempUser.id);
+                let path = route.query.to;
+                if (path == undefined) {
+                    path = '/ahcha';
+                }
+                router.push({ path: path });
+            } catch (err) {
+                console.log(err);
+            } finally {
+                id_input.value = '';
+                pw_input.value = '';
+            }
+        };
+
+        const signUp = function () {
+            router.push('/ahcha/signup');
+        };
+
+        return { id_input, pw_input, login, signUp };
     },
 };
 </script>
--- ahcha-ahcha-app/src/views/SignupView.vue
+++ ahcha-ahcha-app/src/views/SignupView.vue
@@ -1,13 +1,103 @@
 <template lang="">
-    <div>
-        
-    </div>
+    <section style="background-color: #eee">
+        <div class="container h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-lg-12 col-xl-11">
+                    <div class="card text-black mt-5 mb-5" style="border-radius: 25px">
+                        <div class="card-body p-md-5">
+                            <div class="row justify-content-center">
+                                <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                                    <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">회원 가입</p>
+
+                                    <form class="mx-1 mx-md-4">
+                                        <div class="d-flex flex-row align-items-center mb-4">
+                                            <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                                            <div data-mdb-input-init class="form-outline flex-fill mb-0">
+                                                <label>ID</label>
+                                                <input type="text" class="form-control" />
+                                            </div>
+                                        </div>
+                                        <div class="d-flex flex-row align-items-center mb-4">
+                                            <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                                            <div data-mdb-input-init class="form-outline flex-fill mb-0">
+                                                <label>이름</label>
+                                                <input type="text" class="form-control" />
+                                            </div>
+                                        </div>
+
+                                        <div class="d-flex flex-row align-items-center mb-4">
+                                            <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                                            <div data-mdb-input-init class="form-outline flex-fill mb-0">
+                                                <label>이메일</label>
+                                                <input type="email" class="form-control" />
+                                            </div>
+                                        </div>
+                                        <div class="d-flex flex-row align-items-center mb-4">
+                                            <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                                            <div data-mdb-input-init class="form-outline flex-fill mb-0">
+                                                <label>휴대폰 번호</label>
+                                                <input type="text" class="form-control" />
+                                            </div>
+                                        </div>
+
+                                        <div class="d-flex flex-row align-items-center mb-4">
+                                            <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                                            <div data-mdb-input-init class="form-outline flex-fill mb-0">
+                                                <label>비밀번호 입력</label>
+                                                <input type="password" class="form-control" />
+                                            </div>
+                                        </div>
+
+                                        <div class="d-flex flex-row align-items-center mb-4">
+                                            <i class="fas fa-key fa-lg me-3 fa-fw"></i>
+                                            <div data-mdb-input-init class="form-outline flex-fill mb-0">
+                                                <label>비밀번호 다시 입력</label>
+                                                <input type="password" class="form-control" />
+                                            </div>
+                                        </div>
+
+                                        <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                                            <button
+                                                type="button"
+                                                data-mdb-button-init
+                                                data-mdb-ripple-init
+                                                class="btn btn-lg"
+                                                style="background-color: #fbe4a7"
+                                            >
+                                                회원가입
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                                    <img
+                                        src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+                                        class="img-fluid"
+                                        alt="Sample image"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 <script>
+import { reactive, ref } from 'vue';
 export default {
-    
-}
+    setup() {
+        const user_info = reactive({
+            id: '',
+            user_name: '',
+            user_nickname: '',
+            phone_num: '',
+            email: '',
+            image: '',
+            password: '',
+        });
+    },
+};
 </script>
-<style lang="">
-    
-</style>
+<style lang=""></style>
